5af28cff02296a0a1d27ad25d03de11588607055
meraihan <sayedmahmudraihan@gmail.com>
2019-11-06 23:50:48 +0600

    pdf file reads

--- src/test/java/com/rupali/bank/pdftoexcel/ReadPdfTest.java
+++ src/test/java/com/rupali/bank/pdftoexcel/ReadPdfTest.java
@@ -1,22 +1,45 @@
 package com.rupali.bank.pdftoexcel;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ReadPdfTest {
 
-    @Value("${files.location}")
-    String filesLocation;
-
     @Test
     public void pdfReadTest(){
+        try (PDDocument document = PDDocument.load(new File("./files/BSL-01.pdf"))) {
 
+            document.getClass();
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+                PDFTextStripper tStripper = new PDFTextStripper();
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+                // split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
