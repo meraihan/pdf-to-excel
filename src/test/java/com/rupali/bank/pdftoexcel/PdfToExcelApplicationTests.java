044ef2db5f5a3a81a8d9b4d5d70d2d77fe2ee346
meraihan <sayedmahmudraihan@gmail.com>
2019-11-06 23:16:40 +0600

    Initial structure

---
+++ src/test/java/com/rupali/bank/pdftoexcel/PdfToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupali.bank.pdftoexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfToExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
