dd719739b7118faced6e570ef37f8631b6c56697
meraihan <sayedmahmudraihan@gmail.com>
2019-11-24 23:07:24 +0600

    write in csv files

--- src/main/java/com/rupali/bank/pdftoexcel/service/ConvertPdfToExcelService.java
+++ src/main/java/com/rupali/bank/pdftoexcel/service/ConvertPdfToExcelService.java
@@ -6,11 +6,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 @Slf4j
 @Service
@@ -21,27 +27,55 @@ public class ConvertPdfToExcelService {
 
             document.getClass();
             if (!document.isEncrypted()) {
-
                 PDFTextStripperByArea stripper = new PDFTextStripperByArea();
                 stripper.setSortByPosition(true);
                 PDFTextStripper tStripper = new PDFTextStripper();
                 String pdfFileInText = tStripper.getText(document);
-                //System.out.println("Text:" + st);
                 log.info("text: {}", pdfFileInText);
-                // split by whitespace
-                String lines[] = pdfFileInText.split("\\r?\\n");
-                for (String line : lines) {
-                    System.out.println(line);
-                }
+                writeData(pdfFileInText);
             }
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void writeData(List<SampleData> excelDataList) throws Exception {
+    public void writeData(String pdfData) throws Exception {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Blank Sheet");
+        Map<String, Object[]> data = new TreeMap<String, Object[]>();
 
+        SampleData sampleData = new SampleData();
+
+        data.put("1", new Object[] {"DATE", "RECONCILATION DATE", "TRANSACTION", "ACCOUNT", "ISSUER", "DEBIT", "CREDIT", "BALANCE"});
+        data.put("2", new Object[] {""});
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset)
+        {
+            Row row = sheet.createRow(rownum++);
+            Object [] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr)
+            {
+                Cell cell = row.createCell(cellnum++);
+                if(obj instanceof String)
+                    cell.setCellValue((String)obj);
+                else if(obj instanceof Integer)
+                    cell.setCellValue((Integer)obj);
+            }
+        }
+        try
+        {
+            //Write the workbook in file system
+            FileOutputStream out = new FileOutputStream(new File("GL-Ledger.xlsx"));
+            workbook.write(out);
+            out.close();
+            System.out.println("General ledger is written successfully on disk.");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
